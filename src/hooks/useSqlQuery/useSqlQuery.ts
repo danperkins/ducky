2b635b56b46e9d50baf8cf459bdc4a900d18b028
Dan Perkins <dan.perkins@lifeomic.com>
2023-01-17 20:50:52 -0500

    fix: patch handling of Timestamp type data

--- src/hooks/useSqlQuery/useSqlQuery.ts
+++ src/hooks/useSqlQuery/useSqlQuery.ts
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as duckdb from "@duckdb/duckdb-wasm";
+import { DataType } from "apache-arrow";
 
 type QueryState =
   | {
@@ -60,8 +61,22 @@ export const useSqlQuery = (connection?: duckdb.AsyncDuckDBConnection) => {
         // but it doesn't handle all of the data types very well
         table.schema.fields.forEach((field) => {
           temp.labels.push(field.name);
-          let vals = table.getChild(field.name)?.toArray() || [];
-          if (vals instanceof BigInt64Array) {
+          const child = table.getChild(field.name);
+          let vals = child?.toArray() || [];
+
+          // Patched Timestamp type using:
+          // https://observablehq.com/@randomfractals/apache-arrow#cell-530
+          if (DataType.isTimestamp(child?.type)) {
+            const dates = vals;
+            vals = [];
+            for (let i = 0; i * 2 < dates.length; i++) {
+              const date = new Date(
+                (dates[i * 2 + 1] * Math.pow(2, 32) + dates[i * 2]) / 1000
+              );
+              temp.values[i].push(date.toISOString());
+              vals.push(date);
+            }
+          } else if (vals instanceof BigInt64Array) {
             vals.forEach((v: any, i: any) => {
               if (!temp.values[i]) {
                 temp.values[i] = [];
